69ed963a0c68b0b08c772695897d97d609524593
“olaprabal” <prabal.tikeriha@olacabs.com>
2022-07-12 14:24:21 +0530

    java test

--- java/Demo.java
+++ java/Demo.java
@@ -1,5 +1,18 @@
+class Student{
+    int id;
+    String name;
+
+    public void show(){
+        System.out.println(this.id+","+this.name+"\n");
+    }
+}
+
 public class Demo {
     public static void main(String[] args) {
-        System.out.println("Hello to 3 week");
+        // System.out.println("Hello to 3 week");
+        Student obj = new Student();
+        obj.id = 101;
+        obj.name = "Alex";
+        obj.show();
     }
 }
